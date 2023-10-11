231753a626730b6ac77e6228adf33d624902b645
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-11 20:26:59 +0300

    Update loader.lua

--- Libraries/loader.lua
+++ Libraries/loader.lua
@@ -2,10 +2,6 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Panda
 
 local window = library:Window("Window")
 
-window:Toggle("Example toggle", true, function(bool)
-    print(bool) -- bool is true or false depending on the state of the toggle
-end)
-
 window:Button("Button name", function()
    print("pressed button")
 end)
