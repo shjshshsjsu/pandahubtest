525a9205ac1e813025550c9d20c0b4006426ec05
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-11 20:31:21 +0300

    Update loader.lua

--- Libraries/loader.lua
+++ Libraries/loader.lua
@@ -1,7 +1,7 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Panda-Respiratory/Panda-UI-Library/main/Libraries/Main"))()
 
-local window = library:Window("Window")
+local window = library:wreston("Test Gui")
 
-window:Button("Button name", function()
-   print("pressed button")
+wreston:Button("Buton", function()
+   print("Butona Tiklanildi")
 end)
