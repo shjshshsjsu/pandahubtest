6786792943f9e89b3c252b323ab4e83871e70598
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-11 20:25:17 +0300

    Update loader.lua

--- Libraries/loader.lua
+++ Libraries/loader.lua
@@ -1,3 +1,11 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Panda-Respiratory/Panda-UI-Library/main/Libraries/Main"))()
 
 local window = library:Window("Window")
+
+window:Toggle("Example toggle", true, function(bool)
+    print(bool) -- bool is true or false depending on the state of the toggle
+end)
+
+window:Button("Button name", function()
+   print("pressed button")
+end)
