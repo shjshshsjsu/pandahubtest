95986d5afa7c67181a132f74f23ae02157b68d5d
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-13 23:23:11 +0300

    Update loader.lua

--- Libraries/loader.lua
+++ Libraries/loader.lua
@@ -2,14 +2,8 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/shjsh
 
 local window = library:Window("Test")
 
-window:Slider("Example Slider",0,10,20,30,40,50,60,70,80,90,100, function(value)
+window:Slider("Speed"),0,100, function(value)
    print(value)
 
-   local function DefaultWalkSpeed(plr)
-	local humanoid = plr.Parent:FindFirstChild("Humanoid")
-	if humanoid then
-		humanoid.WalkSpeed = (value)
-		
-	end
-      end
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = function(value)
 end)
