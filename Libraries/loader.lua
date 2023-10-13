24f02d246cd08a69834ab7ec4d23985f8ae3f818
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-13 23:18:59 +0300

    Update loader.lua

--- Libraries/loader.lua
+++ Libraries/loader.lua
@@ -4,4 +4,12 @@ local window = library:Window("Test")
 
 window:Slider("Example Slider",0,10,20,30,40,50,60,70,80,90,100, function(value)
    print(value)
+
+   local function DefaultWalkSpeed(plr)
+	local humanoid = plr.Parent:FindFirstChild("Humanoid")
+	if humanoid then
+		humanoid.WalkSpeed = (value)
+		
+	end
+      end
 end)
